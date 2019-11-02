39ba3089ecba87231ada6385b43b82ea35ec6a69
Carrie Hunner <clh87@login-teer-13.oit.duke.edu>
2019-11-02 15:51:27 -0400

    initial project setup

---
+++ EarthAid/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  EarthAid
+//
+//  Created by Carrie Hunner on 11/2/19.
+//  Copyright © 2019 Duke. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ EarthAid/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  EarthAid
+//
+//  Created by Carrie Hunner on 11/2/19.
+//  Copyright © 2019 Duke. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
